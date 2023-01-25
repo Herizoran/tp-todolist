16b771151b22efff744f232e96089a0781cf133a
herizoran <herizo.ran@gmail.com>
2023-01-25 17:14:27 +0300

    fix: change jsx to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import InputField from "./components/InputField";
 import {Todo} from "./model";
+import TodoList from "./components/TodoList";
 
 function App() {
     const [todo, setTodo] = useState<string>("");
-    // const [todos, setTodos] = useState<Todo>([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const handleSubmit = (event:React.FormEvent) => {
         event.preventDefault();
         if (todo){
-            // setTodos([...todos, {id:Data.now(),todo,isDone}])
-            console.log(todo);
-
+            setTodos([...todos, {id:Date.now(),todo,isDone:false}])
             setTodo("");
         }
-
     }
     return (
     <div className="App">
       <span className="heading">TODOLIST</span>
         <InputField todo ={todo} setTodo = {setTodo} handleSubmit = {handleSubmit} />
+        <TodoList todos ={todos} setTodos = {setTodos}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+interface Props {
+    todo:string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleSubmit: (e:React.FormEvent)=> void;
+}
 
-const InputField = ({todo,setTodo,handleSubmit}) => {
+const InputField = ({todo,setTodo,handleSubmit}:Props) => {
   return (
       <form className="input" onSubmit={handleSubmit}>
         <input type = "input"
