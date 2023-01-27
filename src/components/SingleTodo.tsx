ac8ff19a3cd5cf1e1363e8939be4e4ccc678544d
herizoran <herizo.ran@gmail.com>
2023-01-27 23:40:12 +0300

    feat: add handle done funciton

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Todo} from "../model";
+import "./styles.css"
+
 
 type Props = {
   todo:Todo;
@@ -7,9 +9,14 @@ type Props = {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const SingleTodo = ({todo,todos,setTodos}:Props) => {
+    const handleDone = (id:number) => {
+      setTodos(todos.map((todo) =>
+          todo.id === id ? {...todo, isDone: !todo.isDone}: todo
+      ));
+    }
   return (
     <form className="single-todo">
-        <input type={"checkbox"}/>
+        <input type={"checkbox"} onChange={() => handleDone(todo.id)}/>
         <span className="text-todo">
             {todo.todo}
         </span>
