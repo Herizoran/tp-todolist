d070acdee8ba2db8e671806aadbcb15aa3cd1e65
herizoran <herizo.ran@gmail.com>
2023-01-28 00:44:17 +0300

    feat: add filter for todos
    This is about remaping the list of todos
    which means, deleting the current checked todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -2,26 +2,35 @@ import React from 'react'
 import {Todo} from "../model";
 import "./styles.css"
 
-
 type Props = {
   todo:Todo;
   todos:Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const SingleTodo = ({todo,todos,setTodos}:Props) => {
-    const handleDone = (id:number) => {
-      setTodos(todos.map((todo) =>
-          todo.id === id ? {...todo, isDone: !todo.isDone}: todo
-      ));
+    const handleDone = (id: number, event:any) => {
+        setTodos(todos.map((todo) =>
+            todo.id === id ? {...todo, isDone: !todo.isDone}: todo
+        ));
+        if (event.target.checked){
+            setTodos(todos.filter((todo) => todo.id !== id));
+        }
     }
   return (
     <form className="single-todo">
-        <input type={"checkbox"} onChange={() => handleDone(todo.id)}/>
-        <span className="text-todo">
-            {todo.todo}
-        </span>
+        <input type={"checkbox"} onChange={(event) => handleDone(todo.id,event)}/>
+        {
+            todo.isDone ? (
+                <s className="text-todo">
+                    {todo.todo}
+                </s>
+            ):(
+                <span className="text-todo">
+                    {todo.todo}
+                </span>
+            )
+        }
     </form>
   )
 }
-
 export default SingleTodo
